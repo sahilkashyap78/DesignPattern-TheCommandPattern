eec2a9eab123911e195ff75e374dcbe8ae976554
sahilkashyap78 <sahilkashyap78@gmail.com>
2021-03-23 20:53:52 +0530

    Command basic pattern

---
+++ Assets/Scripts/Command.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command 
+{
+    public abstract void Execute(Animator animator);
+}
+
+public class DoJumping:Command
+{
+    public override void Execute(Animator animator)
+    {
+        animator.SetTrigger("isJumping");
+    }
+}
+
+public class DoPunching : Command
+{
+    public override void Execute(Animator animator)
+    {
+        animator.SetTrigger("isPunching");
+    }
+}
+
+public class MoveForward : Command
+{
+    public override void Execute(Animator animator)
+    {
+        animator.SetTrigger("isWalking");
+    }
+}
+
+public class DoKicking : Command
+{
+    public override void Execute(Animator animator)
+    {
+        animator.SetTrigger("isKicking");
+    }
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public GameObject actor;
+    Animator animator;
+    Command keySpace, keyP, keyK, upArrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        keySpace = new DoJumping();
+        keyP = new DoPunching();
+        keyK = new DoKicking();
+        upArrow = new MoveForward();
+        animator = actor.GetComponent<Animator>();
+        Camera.main.GetComponent<CameraFollow360>().player = actor.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            upArrow.Execute(animator);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            keySpace.Execute(animator);
+        }
+        else if(Input.GetKeyDown(KeyCode.P))
+        {
+            keyP.Execute(animator);
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            keyK.Execute(animator);
+        }
+
+    }
+}
